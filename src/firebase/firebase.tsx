aafd89e0f39de237f5612eebe3e64b25e6010be3
Marini23 <marina.smile1m@gmail.com>
2025-03-29 13:44:34 +0100

    add: env file

--- src/firebase/firebase.tsx
+++ src/firebase/firebase.tsx
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 // import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyC-WA9qNWnnMGDVdE_H_9-ujk0icajjdU4",
-  authDomain: "day-coffee-challenge.firebaseapp.com",
-  projectId: "day-coffee-challenge",
-  storageBucket: "day-coffee-challenge.firebasestorage.app",
-  messagingSenderId: "246878071745",
-  appId: "1:246878071745:web:dddf5ad71119a71b719d40",
-  measurementId: "G-T2QL8WJ78P",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
