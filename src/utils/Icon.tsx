4af177d4e09a961777b657e1c12d4ce0405ec578
Marini23 <marina.smile1m@gmail.com>
2025-04-20 20:39:57 +0200

    fix: IconProps

--- src/utils/Icon.tsx
+++ src/utils/Icon.tsx
@@ -1,5 +1,11 @@
-import { IconProps } from "../types/tasks";
 import sprite from "../assets/symbol-defs.svg";
+
+export type IconProps = {
+  name: string;
+  size?: number;
+  className?: string;
+  color?: string;
+};
 
 export const Icon: React.FC<IconProps> = ({ name, size, className, color }) => {
   return (
