d933d487bc845670fa8a43d95f7c2608470c0344
Marini23 <marina.smile1m@gmail.com>
2025-06-29 10:06:27 +0200

    update: share  modal

--- src/components/ShareModal/ShareModal.tsx
+++ src/components/ShareModal/ShareModal.tsx
@@ -9,7 +9,7 @@ import {
 } from "react-share";
 import { Achievement } from "../../types/achievements";
 import { useTranslation } from "react-i18next";
-import { useCallback, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { toPng } from "html-to-image";
 import beansIcon from "../../assets/beans-png.png";
 import cupIcon from "../../assets/cup_png.png";
@@ -32,7 +32,7 @@ export const ShareModal: React.FC<ShareModalProps> = ({
   achievement,
 }) => {
   const { t } = useTranslation();
-  const [showIcons, setShowIcons] = useState<boolean>(true);
+  const [showIcons, setShowIcons] = useState<boolean>(false);
   const [generatedImage, setGeneratedImage] = useState<string | null>(null);
   const [cloudinaryUrl, setCloudinaryUrl] = useState<string | null>(null);
   const cardRef = useRef<HTMLDivElement | null>(null);
@@ -45,22 +45,38 @@ export const ShareModal: React.FC<ShareModalProps> = ({
 
   const selectedIcon = achievementIcons[achievement.id];
 
-  const onButtonClick = useCallback(() => {
-    if (!cardRef.current) return;
+  // const onButtonClick = useCallback(() => {
+  //   if (!cardRef.current) return;
 
-    setShowIcons(false);
+  //   setShowIcons(false);
 
-    toPng(cardRef.current, { cacheBust: true })
-      .then(async (dataUrl) => {
-        setGeneratedImage(dataUrl);
-        const uploadedUrl = await uploadBageToCloudinary(dataUrl);
-        console.log(uploadedUrl);
-        setCloudinaryUrl(uploadedUrl);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  }, []);
+  //   toPng(cardRef.current, { cacheBust: true })
+  //     .then(async (dataUrl) => {
+  //       setGeneratedImage(dataUrl);
+  //       const uploadedUrl = await uploadBageToCloudinary(dataUrl);
+  //       console.log(uploadedUrl);
+  //       setCloudinaryUrl(uploadedUrl);
+  //     })
+  //     .catch((err) => {
+  //       console.error(err);
+  //     });
+  // }, []);
+
+  useEffect(() => {
+    if (isOpen && cardRef.current && !generatedImage) {
+      toPng(cardRef.current, { cacheBust: true })
+        .then(async (dataUrl) => {
+          setGeneratedImage(dataUrl);
+          const uploadedUrl = await uploadBageToCloudinary(dataUrl);
+          setCloudinaryUrl(uploadedUrl);
+          setShowIcons(true);
+        })
+        .catch((err) => {
+          console.error(err);
+          setShowIcons(true); // fallback to showing buttons anyway
+        });
+    }
+  }, [isOpen, generatedImage]);
 
   const onShareTelegram = () => {
     if (cloudinaryUrl) {
@@ -84,27 +100,27 @@ export const ShareModal: React.FC<ShareModalProps> = ({
         </button>
         <div
           ref={cardRef}
-          className="bg-[url('/src/assets/share_bg.jpg')] rounded-xl p-6 w-80 h-120 shadow-lg "
+          className="bg-[url('/src/assets/share_bg.jpg')]  py-6 px-2 w-80 h-120 shadow-lg "
         >
           <img
             src={selectedIcon}
             alt="Achievement Icon"
-            className="w-35 h-50 mx-auto mt-10 mb-10 "
+            className="w-40 h-50 mx-auto mt-10 mb-10 "
           />
-          <h2 className="text-[22px]  font-semibold mb-10 text-secondary text-center">
+          <h2 className="text-[22px]  font-semibold  text-secondary text-center ">
             {t(`achievements.share_text`)}
             <br />{" "}
-            <span className="text-gold font-bold">
+            <span className="text-gold font-bold text-[20px]">
               {t(`achievements.${achievement.id}`).toUpperCase()}
             </span>
           </h2>
           {showIcons && (
-            <div className="flex justify-center gap-3">
+            <div className="flex justify-center gap-3 mt-4 mb-auto">
               <FacebookShareButton url={url}>
                 <FacebookIcon size={30} round />
               </FacebookShareButton>
 
-              <button onClick={cloudinaryUrl ? onShareTelegram : onButtonClick}>
+              <button onClick={onShareTelegram}>
                 <TelegramIcon size={30} round />
               </button>
 
@@ -117,7 +133,7 @@ export const ShareModal: React.FC<ShareModalProps> = ({
               </LinkedinShareButton>
             </div>
           )}
-          {generatedImage && (
+          {/* {generatedImage && (
             <div className="mt-4 fixed top-0">
               <h3 className="text-center text-secondary mb-2">
                 {t("Preview")}
@@ -137,7 +153,7 @@ export const ShareModal: React.FC<ShareModalProps> = ({
                 </a>
               </div>
             </div>
-          )}
+          )} */}
         </div>
       </div>
     </div>
