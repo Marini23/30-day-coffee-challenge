8d39749e66fd7d4624bc7ef011fdd22dfd034e0b
Marini23 <marina.smile1m@gmail.com>
2025-08-22 18:35:02 +0200

    update policy link

--- src/components/SignUpForm/SignUpForm.tsx
+++ src/components/SignUpForm/SignUpForm.tsx
@@ -167,14 +167,12 @@ export const SignUpForm: React.FC = () => {
             </div>
             <p className="my-0 mx-auto px-8 text-center w-full text-[12px]  text-espresso desktop:text-[16px]">
               {t("signUpForm.acceptRules")}{" "}
-              <a
-                href="/30-day-coffee-challenge/privacy-policy"
+              <Link
+                to="/privacy-policy"
                 className="font-bold text-espresso hover:underline"
-                target="_blank"
-                rel="noopener noreferrer"
               >
                 {t("signUpForm.linkPolicy")}
-              </a>
+              </Link>
             </p>
             <button
               type="submit"
